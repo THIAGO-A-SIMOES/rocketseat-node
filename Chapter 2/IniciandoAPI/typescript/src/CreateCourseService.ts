8f8206d5ff54692de519ce2e94a24a6477b99aa8
Thiago Simoes <simoes.cpd@gmail.com>
2022-07-21 13:36:15 -0300

    Jornada Ignite Rocketset - NodeJS - Chapter 2 Configurando-TS-Node-Dev

---
+++ Chapter 2/IniciandoAPI/typescript/src/CreateCourseService.ts
@@ -0,0 +1,14 @@
+interface Course {
+    name: String;
+    duration?: Number;
+    educator: String;
+}
+
+class CreateCourseService {
+    execute({ duration = 8, educator, name }: Course) {
+        console.log(name, duration, educator)
+    }
+}
+
+
+export default new CreateCourseService()
